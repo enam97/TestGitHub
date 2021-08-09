0054ceae402ef05878decf6b02b41ffea9c837ff
“Salma” <h.n222@yahoo.com>
2021-08-08 21:52:08 -0400

    Collaboration.java class added to the project

---
+++ MavenGitHubTest/src/test/java/com/technosoft/selenium/MavenTest/Collaboration.java
@@ -0,0 +1,11 @@
+package com.technosoft.selenium.MavenTest;
+
+public class Collaboration {
+
+
+    public static void main(String[] args) {
+        System.out.println("Salmas collaboration test");
+    }
+
+
+}
